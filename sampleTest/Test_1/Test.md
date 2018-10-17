468645401cca1e0d08b941031bba8dc160634cbc
Bretana <lebretana@inf.ufpel.edu.br>
2018-10-17 09:30:56 -0300

    Test sample

---
+++ sampleTest/Test_1/Test.md
@@ -0,0 +1,14 @@
+## Sample test for ippd - 2018/2
+
+### Question #1: explain the Amdahl's law.
+Is an estimative of how a computacional system will improve its perfomance if there is an improvment in a single part of it.
+In the parallesl compution field, it is used to predict the performance improvment in extracting concurrency from the application.
+
+### Question #2: exemplify and justify why a problem better fits the shared memory model and heterogenous architecture.
+Any problem that uses the SIMD model with small data portion (like big matrixes multiplication) can be nicelly fit into an heterogenous architecture.
+Therefore it uses the shared memory model, wich better will have a faster syncrony than sending many, many messages (distributed memory model mainly uses message passing for syncronization or some abstraction for this model).
+
+### Question #3: using MPI create an application to caculate prime numbers. It should be possible to define (at compile or runtime) the numbers of primes to find.
+Answer in question3.{c, h}
+
+### Question #4:
---
+++ sampleTest/Test_1/question3.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <mpi.h>
+
+#define N_PRIME 100
+#ifndef FALSE
+#define FALSE 0
+#define TRUE !FALSE
+#endif
+
+short prime_test(unsigned p) {
+	for(unsigned i=2 ; i<p; ++i) {
+		if (p%i != 0)
+			continue;
+		return FALSE;
+	}
+	return TRUE;
+}
+
+void print_prime(unsigned p, unsigned pid) {
+	printf("\nFound by process %u, #%u is a prime number!", pid, p);
+}
+
+int main(int argc, char **argv) {
+	
+	unsigned n_prime = N_PRIME;
+	if (argc >= 2)
+		n_prime = (unsigned) atoi(argv[1]);
+
+	int sz, pid = 0;
+	{
+		MPI_Init(&argc, &argv);
+		MPI_Comm_size(MPI_COMM_WORLD, &sz);
+		MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+
+		if (pid++ == 0) {
+			unsigned prime = 0U;
+				MPI_Irecv((void *)&prime, 1, MPI_UNSIGNED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, NULL);
+			for (unsigned i=pid ; i<n_prime ; i+=sz) {
+				printf("\npid: %u -> test %u", pid, i);
+				short is = prime_test(i);
+				if (is)
+					print_prime(i, pid);
+			}
+		} else {
+			for (unsigned i=pid ; i<n_prime ; i+=sz) {
+				//printf("\npid: %u -> test %u", pid, i);
+				short is = prime_test(i);
+				if (is) {
+				// send answer
+				}
+			}
+		}
+
+
+		MPI_Finalize();
+	}
+
+	return 0;
+}
+
